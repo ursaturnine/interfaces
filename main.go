0ce1b6c37366b27f7d2df00e7aef84aea33e4995
Tyrah <tyrah96@gmail.com>
2023-02-05 10:58:42 -0800

    implenting bot interface using an english bot struct and spanish bot struct

--- main.go
+++ main.go
@@ -1,3 +1,35 @@
 package main
 
 import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// this function is of type, bot
+func (englishBot) getGreeting() string {
+	// very custom logic for English greeting
+	return "Hi, There!"
+}
+
+// this function is of type, bot
+func (spanishBot) getGreeting() string {
+	// very custom logic for Spanish greeting
+	return "Hola!"
+}
